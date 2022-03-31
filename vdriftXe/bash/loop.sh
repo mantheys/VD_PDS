c11333e44525f751b1be70261b878a4fc5164dbf
Sergio Manthey Corchado <smanthey@dunegpvm15.fnal.gov>
2022-03-31 07:38:43 -0500

    UPDATE

--- vdriftXe/bash/loop.sh
+++ vdriftXe/bash/loop.sh
@@ -11,9 +11,10 @@ step_x=0.325;step_y=0.325;step_z=1.0
 for x in $(LC_ALL=en_US.UTF-8 seq $i_x $step_x $f_x) ; do
     for y in $(LC_ALL=en_US.UTF-8 seq $i_y $step_y $f_y) ; do
         for z in $(LC_ALL=en_US.UTF-8 seq $i_z $step_z $f_z) ; do
-            jobsub_submit -G dune -Q --role=Analysis -N 1 --expected-lifetime=2h --memory=500MB --disk=1GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --OS=SL7 file:///pnfs/dune/scratch/users/smanthey/vdriftAr/point.sh $x $y $z
+            jobsub_submit -G dune -Q --role=Analysis -N 1 --expected-lifetime=2h --memory=500MB --disk=1GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --OS=SL7 file:///pnfs/dune/scratch/users/smanthey/vdriftXe/point.sh $x $y $z
         done
     done
 done
 
-mv -- "run/arapuca"* "/nashome/s/smanthey/standalone/VD_PDS/vdriftAr/run/"
+mkdir "/nashome/s/smanthey/standalone/RUN_Xe"
+mv -- "run/arapuca"* "/nashome/s/smanthey/standalone/RUN_Xe/"
--- vdriftXe/bash/point.sh
+++ vdriftXe/bash/point.sh
@@ -8,9 +8,9 @@ setup cmake v3_13_1
 setup root v6_18_02a -q e17:prof
 setup geant4 v4_10_3_p03e -q e17:prof
 
-ifdh cp -D /pnfs/dune/scratch/users/smanthey/vdriftAr/vdriftAr_tar.tgz ./
-tar -xzvf vdriftAr_tar.tgz
+ifdh cp -D /pnfs/dune/scratch/users/smanthey/vdriftXe/vdriftXe_tar.tgz ./
+tar -xzvf vdriftXe_tar.tgz
 chmod u+x vdrift_build/g4workshop
 ./vdrift_build/g4workshop $1 $2 $3
-eval "$(echo "ifdh cp -D arapuca* /pnfs/dune/scratch/users/smanthey/vdriftAr/run/.")"
+eval "$(echo "ifdh cp -D arapuca* /pnfs/dune/scratch/users/smanthey/vdriftXe/run/.")"
 
