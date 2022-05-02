fcaeba9ba94aa248f1781963d7cb4b2553ce31e4
Sergio Manthey Corchado <smanthey@dunegpvm15.fnal.gov>
2022-05-02 04:46:18 -0500

    UPDATE

--- layout_files/4x20/vspace_13/Ar/run_loop.sh
+++ layout_files/4x20/vspace_13/Ar/run_loop.sh
@@ -3,7 +3,7 @@ i_x=0.16875
 i_y=-3.0875
 f_x=6.58125
 f_y=3.0875
-step_x=0.05
+step_x=0.3375
 step_y=0.325
 step_z=0.25
 #step_x=6.75
@@ -13,7 +13,7 @@ f_z=3.0
 for x in $(LC_ALL=en_US.UTF-8 seq $i_x $step_x $f_x) ; do
     for y in $(LC_ALL=en_US.UTF-8 seq $i_y $step_y $f_y) ; do
 	for z in $(LC_ALL=en_US.UTF-8 seq $i_z $step_z $f_z) ; do
-            jobsub_submit -G dune -Q --role=Analysis -N 1 --expected-lifetime=2h --memory=500MB --disk=1GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --OS=SL7 file:///pnfs/dune/scratch/users/smanthey/vdriftAr/vspace/4x20/vspace_15/run_point.sh $x $y $z
+            jobsub_submit -G dune -Q --role=Analysis -N 1 --expected-lifetime=2h --memory=500MB --disk=1GB --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --OS=SL7 file:///pnfs/dune/scratch/users/smanthey/vdriftAr/vspace/4x20/vspace_13/run_point.sh $x $y $z
 	done
     done
 done
